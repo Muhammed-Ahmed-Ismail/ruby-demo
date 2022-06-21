a62905d4f449f1c689f0f0423a7f323181f206e2
Muhammed-Ismail <muhammadahmed.free@gmail.com>
2022-06-21 22:32:02 +0200

    lab-1

---
+++ lab1/lab1.rb
@@ -0,0 +1,60 @@
+# problem 1
+def RepaeteMyChar(char, n)
+    puts char*n
+end
+
+
+#  problem 2
+
+def is_startingWithIf(str)
+
+ puts (str[0] == 'i' && str[1] == 'f')?  "true" :  "false"
+end
+
+# problem 3
+
+def swapLastAndFirstChars(str)
+    tempStr = str.dup
+    str[str.length-1] = str[0]
+    str[0] = tempStr[str.length-1]
+    return str
+end
+
+
+
+#  problem 4
+
+def addFirstCharInStrBeginAndLast(str)
+  if str.length()>=1
+    str[0] + str + str[0]
+  elsif 
+    puts "Too short string !! "
+  end
+
+end
+
+
+
+
+
+# problem 5
+def IsLeapYear(year)
+  if (year % 100 == 0 && year % 400 ==0) || (year % 4 == 0 && year % 100 != 0)
+      puts year.to_s + ' is a leap year'
+  end
+end
+
+
+
+# problem 7
+
+def sumTill17(array)
+  sum = 0
+  for n in array
+    if n == 17
+      break
+    end
+    sum += n
+  end
+  return sum
+end
