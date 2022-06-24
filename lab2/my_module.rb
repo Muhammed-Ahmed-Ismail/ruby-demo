8c40331198f3b956016a12466f3c0614bacfb014
Muhammed-Ismail <muhammadahmed.free@gmail.com>
2022-06-24 19:49:08 +0200

    use initialize

--- lab2/my_module.rb
+++ lab2/my_module.rb
@@ -2,10 +2,13 @@ require 'date'
 module MyModule
 
   class Person
-    @fname = 'fname'
-    @lname = 'lname'
-    @birth_date = Date.parse('2000-01-01')
-    # def initialize (fname = 'fname', lname = 'lname', birth_date = Date.new) end
+
+    def initialize (fname = 'fname', lname = 'lname', birth_date = Date.parse('2000-01-01'))
+      @fname = fname
+      @lname = lname
+      @birth_date = birth_date
+    end
+
     def getData
       puts 'Hi Enter Your First Name: '
       @fname = gets.chomp
@@ -17,13 +20,13 @@ module MyModule
     end
 
     def printData
-      age_in_years = (@age.to_i/365).floor
+      age_in_years = (@age.to_i / 365).floor
       age_in_days = (@age).to_i
-      age_in_monthes = age_in_years*12 + ((age_in_days-(age_in_years*365+(age_in_years/4).floor))/30).floor
+      age_in_monthes = age_in_years * 12 + ((age_in_days - (age_in_years * 365 + (age_in_years / 4).floor)) / 30).floor
       puts 'Hi ' + @fname + @lname
-      puts  "Your Age in Years is  #{age_in_years}"
-      puts  "Your Age in months is  #{age_in_monthes}"
-      puts  "Your Age in Days is  #{age_in_days}"
+      puts "Your Age in Years is  #{age_in_years}"
+      puts "Your Age in months is  #{age_in_monthes}"
+      puts "Your Age in Days is  #{age_in_days}"
     end
   end
 end
